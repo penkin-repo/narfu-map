9e5979b65c71db7ba0f97ea86c0ed5b887424f81
penkin-repo <422000@list.ru>
2022-08-01 03:19:40 +0300

    s download

--- js/script.js
+++ js/script.js
@@ -3,46 +3,39 @@ const scaleMaster = document.querySelector(".scale-master");
 
 const sm100 = document.querySelector("#sm100");
 const func100 = () => {
-  console.log("click 1");
   scaleMaster.style.width = "100%";
 };
 sm100.addEventListener("click", func100);
 
 const sm130 = document.querySelector("#sm130");
 const func130 = () => {
-  console.log("click 2");
   scaleMaster.style.width = "130%";
 };
 sm130.addEventListener("click", func130);
 
 const sm160 = document.querySelector("#sm160");
 const func160 = () => {
-  console.log("click 3");
   scaleMaster.style.width = "160%";
 };
 sm160.addEventListener("click", func160);
 //------------------------
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".btn-scale");
 buttons.forEach((element) => {
   element.addEventListener("click", funcActive);
 });
 function funcActive(e) {
   if (!e.target.classList.contains("active")) {
-    console.log('true');
-    
     buttons.forEach((element) => {
       element.classList.remove("active");
     });
     e.target.classList.add("active");
   } else {
     // console.log('true');
-    
     // buttons.forEach((element) => {
     //   element.classList.remove("active");
     // });
     // e.target.classList.add("active, bbb");
   }
-
 
   //   buttons.classList.toggle("active");
   //   buttons.forEach((element) => {
